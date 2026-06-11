ad1a9d1de9c34f3f56a86027d1eb0603e9396b7d
nathdixit340-spec <nathdixit340@gmail.com>
2026-06-11 22:26:11 +0530

    Update newbot.py

--- newbot.py
+++ newbot.py
@@ -12,7 +12,7 @@ JOIN_CHANNEL_LINK = "https://t.me/RavenCheckerV2"
 FORCE_JOIN_IMAGES = []
 
 # APIs – UPDATE WITH YOUR RAILWAY API URL
-SHOPIFY_API_URL = "https://shopapi-production-f0cc.up.railway.app"
+SHOPIFY_API_URL = "https://shopapi-production-1a82.up.railway.app/shopify"
 RAZORPAY_API_URL = "https://razorpayapi-production.up.railway.app/razorpay"
 
 # Worker limits
